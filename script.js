d7ebf094e04e57f4da88d055f92f1612f4a861e1
Arthur Otavio Rodrigues Davidonis <aod.otavio@gmail.com>
2023-08-15 23:58:58 -0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+function infoAlert(){
+ var conf = prompt("Tem certeza de que as informações estão corretas? digite: s/n")
+    if(conf == "s" || conf == "sim" || conf == "Sim"){
+       alert("Obrigado por prencher este formulario! Já recebemos suas //informações.")
+
+     }
+   
+   else{
+       alert("Tente inserir os dados novamente!")
+   }
+}
+
+
